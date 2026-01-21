2275cea38d5b88f8482f9f62417f9054ebbc82a0
FranciscoPS <francisco.ps1996@gmail.com>
2026-01-21 15:05:20 -0600

    feat: basic setup

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private Vector3 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+        
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            rb.linearVelocity = new Vector3(moveDirection.x * moveSpeed, rb.linearVelocity.y, moveDirection.z * moveSpeed);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector3(0f, rb.linearVelocity.y, 0f);
+        }
+    }
+}
