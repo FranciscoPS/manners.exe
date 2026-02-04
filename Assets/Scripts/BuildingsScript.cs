ab61540ba846a17a81b3ce4f966484fd70dec51a
M-A-R-M <mauricio.reyna@saei.mx>
2026-02-03 19:15:37 -0600

    BulidingsDestruction
    Buildings destruction added

--- Assets/Scripts/BuildingsScript.cs
+++ Assets/Scripts/BuildingsScript.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using System.Collections;
 
 public class BuildingsScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Destruction Settings")]
+    [SerializeField] private float sinkSpeed = 1.5f;
+    [SerializeField] private float sinkDuration = 2f;
+
+    private bool isDestroying = false;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player") && !isDestroying)
+        {
+            isDestroying = true;
+            StartCoroutine(SinkAndDestroy());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SinkAndDestroy()
     {
-        
+        float elapsedTime = 0f;
+
+        while (elapsedTime < sinkDuration)
+        {
+            transform.Translate(Vector3.down * sinkSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
