6de62bd05eb9f0c7259eba27403b45213415c4a0
M-A-R-M <mauricio.reyna@saei.mx>
2026-02-09 21:21:17 -0600

    Shop scirpt fix
    Access to the shop now stops time

--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -1,19 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ShopScript : MonoBehaviour
 {
     [Header("UI")]
     [SerializeField] private GameObject interactionText;
 
+    private bool playerInRange = false;
+    private bool shopOpen = false;
+
+    private InputAction openShopAction;
+
+    private void Awake()
+    {
+        openShopAction = new InputAction(
+            name: "OpenShop",
+            binding: "<Keyboard>/p"
+        );
+    }
+
+    private void OnEnable()
+    {
+        openShopAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        openShopAction.Disable();
+    }
+
     private void Start()
     {
         interactionText.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (playerInRange && !shopOpen && openShopAction.triggered)
+        {
+            OpenShop();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            playerInRange = true;
             interactionText.SetActive(true);
         }
     }
@@ -22,7 +55,16 @@ public class ShopScript : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            playerInRange = false;
             interactionText.SetActive(false);
         }
+    }
+    private void OpenShop()
+    {
+        shopOpen = true;
+
+        Time.timeScale = 0f;
+
+        interactionText.SetActive(false);
     }
 }
